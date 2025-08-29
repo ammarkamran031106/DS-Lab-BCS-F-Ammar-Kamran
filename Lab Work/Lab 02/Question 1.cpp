0fc751becea2ce4958ac4c793d60868290ea33a8
Ammar kamran ali <ammarkamran2006@gmail.com>
2025-08-29 11:57:26 +0500

    Update Question 1.cpp

--- Lab Work/Lab 02/Question 1.cpp
+++ Lab Work/Lab 02/Question 1.cpp
@@ -1,1 +1,38 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+    int *arr;
+    int size;
+    cout<<"ENTER SIZE OR ARRAY: "<<endl;
+    cin>>size;
+    arr=new int[size];
+    cout<<"INTIALIZING IT BY ZERO "<<endl;
+    for(int i=0;i<size;i++){
+        arr[i]=0;
+    }
+    int ele;
+    cout<<"Enter the no of element u want to edit "<<endl;
+    cin>>ele;
+    if(ele>size){
+        cout<<"no possible" <<endl;
+    }
+    else{
+    for(int i=0;i<ele;i++){
+    int pos;    
+    int val;
+    cout<<"Enter postion and val "<<endl;
+    cin>>pos>>val;
+    if(pos<0 || pos>=size){
+        cout<<"out of bound "<<endl;
+    }
+    else{
+    arr[pos]=val;
+    }
+    }
+    }
+    cout<<"PRINTING the array "<<endl;
+    for(int i=0; i<size;i++){
+        cout<<arr[i]<<"         ";
+    }
+    
+}
