3a2c2f53ce69f6b9a7444b7f250ef87c7efb9d4b
Ammar kamran ali <ammarkamran2006@gmail.com>
2025-09-18 23:59:32 +0500

    Update Question 04.cpp

--- Lab Work/Lab 04/Post Lab/Question 04.cpp
+++ Lab Work/Lab 04/Post Lab/Question 04.cpp
@@ -12,6 +12,23 @@ int *Insertion_Sort(int *arr,int size){
 	}
 return arr;
 }
+int Binary_Search(int *a,int size,int target){
+	int left=0;
+	int right=size-1;
+	while(left<=right){
+		int mid=left+(right-left)/2;
+		if(a[mid]==target){
+			return mid;
+		}
+		else if(a[mid]<target){
+			left=mid+1;
+		}
+		else{
+			right=mid-1;
+		}
+	}
+	return -1;
+}
 int main(){
 	int *arr;
 	cout<<"Enter size of array: ";
@@ -28,5 +45,16 @@ int main(){
 	cout<<"Sorted Array"<<endl;
 	for(int i=0;i<size;i++){
 		cout<<arr[i]<<"    " ;
+	}
+	int target;
+	cout<<endl;
+	cout<<"Enter the target u want to find in this sorted array :";
+	cin>>target;
+	int x=Binary_Search(a,size,target);
+	if(x >-1){
+		cout<<"Element Found at index "<<x+1<<" having value "<<a[x]<<endl;
+	}
+	else{
+		cout<<"Element Not Found In Array "<<endl;
 	}
 }
