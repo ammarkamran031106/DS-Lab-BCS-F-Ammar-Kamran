e90abd631d6a80527acfb915a0ff55835d12eca4
Ammar kamran ali <ammarkamran2006@gmail.com>
2025-09-19 12:16:26 +0500

    Create Question 01.cpp

---
+++ Lab Work/Lab 05/In Lab/Question 01.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int Factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+    return n * Factorial(n - 1);
+}
+
+int main() {
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+
+    cout << "Factorial of " << number << " is " << Factorial(number) << endl;
+
+    return 0;
+}
