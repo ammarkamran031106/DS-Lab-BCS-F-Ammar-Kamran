4d3b7aaca528bd1a926241ba5f0d874b0633eb65
Ammar kamran ali <ammarkamran2006@gmail.com>
2025-10-02 00:48:44 +0500

    Create Question 04.cpp

---
+++ Lab Work/Lab 05/Post Lab/Question 04.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int ackermann(int m,int n){
+	if(m==0){
+		return n+1;
+	}
+	else if(m > 0 && n==0){
+		return ackermann(m-1,1);
+	}
+	else{
+		return ackermann(m-1,ackermann(m,n-1));
+	}
+}
+int main(){
+	int x=ackermann(2,3);
+	cout<<x<<endl;
+}
